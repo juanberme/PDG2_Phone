cf38ce5e475a27e4e73c67923f574e153d443ded
Juan Bermeo <juanbermeo@Compu-de-Bermeo.local>
2023-03-28 17:04:42 -0500

    third commit

--- src/routes/Tags.jsx
+++ src/routes/Tags.jsx
@@ -1,24 +1,35 @@
 import { Tag } from 'primereact/tag';
+import { useState } from 'react';
 import '../styles/tagPage.css'
 
 export default function Tags(){
+
+
+    const [value, setValue] = useState('');
+   
+
+    const handleClickTag = (option) => {
+        setValue(option)
+    }
+
     return <div>
         <p>Escoge las <strong>5 palabras</strong> que más te definen en menos de</p>
         <div className="clock">
 
         </div>
         <div className="tags">
-            <Tag className='tag' value="Casero(a)" rounded/>
-            <Tag className='tag' style={{padding: '12px'}} value="Fiestero(a)" rounded/>
-            <Tag className='tag' style={{padding: '12px'}} value="Tímido(a)" rounded/>
-            <Tag className='tag' style={{padding: '12px'}} value="Elocuente(a)" rounded/>
-            <Tag className='tag' style={{padding: '12px'}} value="Reservado(a)" rounded/>
-            <Tag className='tag' style={{padding: '12px'}} value="Influenciador(a)" rounded/>
-            <Tag className='tag' style={{padding: '12px'}} value="Introvertido(a)" rounded/>
-            <Tag className='tag' style={{padding: '12px'}} value="Extorvertido(a)" rounded/>
-            <Tag className='tag' style={{padding: '12px'}} value="Solitario(a)" rounded/>
-            <Tag className='tag' style={{padding: '12px'}} value="Amiguero(a)" rounded/>
+            <Tag className='tag' onClick={() => handleClickTag("Casero(a)")} value="Casero(a)" rounded/>
+            <Tag className='tag' onClick={() => handleClickTag("Fiestero(a)")} value="Fiestero(a)" rounded/>
+            <Tag className='tag' onClick={() => handleClickTag("Tímido(a)")} value="Tímido(a)" rounded/>
+            <Tag className='tag' onClick={() => handleClickTag("Elocuente(a)")} value="Elocuente(a)" rounded/>
+            <Tag className='tag' onClick={() => handleClickTag("Reservado(a)")} value="Reservado(a)" rounded/>
+            <Tag className='tag' onClick={() => handleClickTag("Influenciador(a)")} value="Influenciador(a)" rounded/>
+            <Tag className='tag' onClick={() => handleClickTag("Introvertido(a)")} value="Introvertido(a)" rounded/>
+            <Tag className='tag' onClick={() => handleClickTag("Extorvertido(a)")} value="Extorvertido(a)" rounded/>
+            <Tag className='tag' onClick={() => handleClickTag("Solitario(a)")} value="Solitario(a)" rounded/>
+            <Tag className='tag' onClick={() => handleClickTag("Amiguero(a)")} value="Amiguero(a)" rounded/>
         </div>
+        <p>Selected option: {value}</p>
     </div>
     
 }
