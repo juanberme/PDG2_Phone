40a6e6a4754c03ce714fab09e090784a377e16ab
Alejandro <alejandrobrand28@gmail.com>
2023-04-01 07:29:55 -0500

    commit: mediaquiaries v0.0.1

--- src/routes/Begin.jsx
+++ src/routes/Begin.jsx
@@ -1,10 +1,15 @@
 import { Button } from 'primereact/button';
 import { PDGButton } from '../components/PDGButton.js';
 import '../styles/beginPage.css';
+import '../styles/mediaqueries.css';
 
 export default function Begin(){
-    return <div>
-        <h1>Estás a punto de entrar a una experiencia distinta</h1>
-        <PDGButton/>
+    return <div className='fullCont'>
+        <section className='fullCont_Ttl'>
+            <h1 id='magic'>Estás a punto de entrar a una experiencia distinta</h1>
+        </section>
+        <section className='fullCont_Btn'>
+            <PDGButton/>
+        </section>
     </div>
 }
