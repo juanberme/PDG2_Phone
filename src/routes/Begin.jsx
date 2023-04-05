9c6dee59b7efde1a2f94a1c6eb316da63db63ac3
Alejandro <alejandrobrand28@gmail.com>
2023-04-05 06:07:50 -0500

    Mediaqueries: correccion de pantalla Begin v0.3.1

--- src/routes/Begin.jsx
+++ src/routes/Begin.jsx
@@ -1,20 +1,28 @@
 import { Button } from 'primereact/button';
 import { PDGButton } from '../components/PDGButton.js';
+
 import '../styles/beginPage.css';
+import '../styles/mediaqueriesBegin.css';
 
 export default function Begin(){
     return <div className='fullCont'>
         <section className='fullCont_Ttl'>
-            <h1 className='fullCont_Ln' id='l1'>¿Te conozco?</h1>
-            <h2 className='fullCont_Ln' id='l2'>¿Nos hemos </h2>
-            <h2 className="fullCont_Ln" id='l3'>visto antes?</h2>
-            <h1 className="fullCont_Ln" id='l4'>Bienvenido,</h1>
-            <h3 className="fullCont_Ln" id="l5">te invito a que juntos</h3>
-            <h2 className="fullCont_Ln" id="l6">experimentemos</h2>
-            <h3 className="fullCont_Ln" id="l7">esta nueva forma</h3>
-            <h2 className="fullCont_Ln" id="l8">de conocernos</h2>
-            <h1 className="fullCont_Ln" id="l9">Toca aquí</h1>
-            <h1 className="fullCont_Ln" id="l0">para continuar</h1>
+            <div className='fullCont_Ln'>
+                <h1 id='l1'>¿Te conozco?</h1>
+                <h2>¿Nos hemos </h2>
+                <h2 id='l2'>visto antes?</h2>
+            </div>
+            <div className='fullCont_Ln'>
+                <h1 id='l3'>Bienvenido,</h1>
+                <h3>te invito a que juntos</h3>
+                <h2 id='l4'>experimentemos</h2>
+                <h3>esta nueva forma</h3>
+                <h2 id='l5'>de conocernos</h2>
+            </div>
+            <div className='fullCont_Ln'>
+                <h1>Toca aquí</h1>
+                <h1 id='l6'>para continuar</h1>
+            </div>
         </section>
         <section className='fullCont_Btn'>
             <PDGButton/>
