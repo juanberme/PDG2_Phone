e521f91b60bd152de9ed2ac802fb940e117e234d
Alejandro <alejandrobrand28@gmail.com>
2023-05-08 06:39:31 -0500

    DISEÑO: Figma-Diseño de pantalla & Arreglos de css y jsx

--- src/routes/prueba.jsx
+++ src/routes/prueba.jsx
@@ -1,5 +1,19 @@
+import '../styles/pruebaPage.css';
+
 export default function Prueba(){
-    return <div>
-        <h1>Prueba</h1>
-    </div>
+    return <section className="CLP_CONT">
+
+        <section className="clp_Banner">
+            <div className="Banner_Ttl">
+                <span className="Ttl_h1">
+                    <h1 id='hs'>Bienvenido,</h1>
+                </span>
+                <span className="Ttl_h3">
+                    <h3 id='hs'>nombre de la empresa</h3>
+                </span>
+            </div>
+        </section>
+
+        <section className="clp_List"></section>
+    </section>
 }
