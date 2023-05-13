616c0b9ecbeb1938aac2543c6829f0ea3dac8d7d
Alejandro <alejandrobrand28@gmail.com>
2023-05-13 11:50:05 -0500

    DISEÑO: Página Final + CSS a 50%

--- src/routes/Final.jsx
+++ src/routes/Final.jsx
@@ -1,7 +1,164 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { Button } from 'primereact/button';
+import { Divider } from 'primereact/divider';
+import { Chart } from 'primereact/chart';
+//import { Colors } from 'chart.js';
+
+import '../styles/FinalPage.css';
 
 export default function Final(){
-    return <div>
-        <h1>Final</h1>
-    </div>
+    /*const [chartData, setChartData] = useState({});
+    const [chartOptions, setChartOptions] = useState({});
+
+    useEffect(() => {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+        const data = {
+            datasets: [{
+                data: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+                backgroundColor: [
+                    documentStyle.getPropertyValue('--red-500'),
+                    documentStyle.getPropertyValue('--green-500'),
+                    documentStyle.getPropertyValue('--yellow-500'),
+                    documentStyle.getPropertyValue('--bluegray-500'),
+                    documentStyle.getPropertyValue('--blue-500'),
+                    documentStyle.getPropertyValue('--cyan-500'),
+                    documentStyle.getPropertyValue('--pink-500'),
+                    documentStyle.getPropertyValue('--brown-500'),
+                    documentStyle.getPropertyValue('--gray-500'),
+                    documentStyle.getPropertyValue('--orange-500'),
+                    documentStyle.getPropertyValue('--lime-500'),
+                    documentStyle.getPropertyValue('--magenta-500'),
+                    documentStyle.getPropertyValue('--red-500'),
+                    documentStyle.getPropertyValue('--blue-500'),
+                    documentStyle.getPropertyValue('--yellow-500'),
+                    documentStyle.getPropertyValue('--green-500'),
+                ],
+                label: 'DataBase'
+            }],
+            labels: ['INFP', 'INFJ', 'INTP', 'INTJ', 'ISFP', 'ISFJ', 'ISTP', 'ISTJ', 'ENFP', 'ENFJ', 'ENTP', 'ENTJ', 'ESFP', 'ESFJ', 'ESTP', 'ESTJ']
+        };
+
+        const options = {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                r: {
+                    grid: {
+                        color: surfaceBorder
+                    }
+                }
+            }
+        };
+
+        setChartData(data);
+        setChartOptions(options);
+    }, []);*/
+
+    const [chartData, setChartData] = useState({});
+    const [chartOptions, setChartOptions] = useState({});
+
+    useEffect(() => {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+        const data = {
+            datasets: [
+                {
+                    data: [10, 15, 10, 11, 17, 20, 21, 25],
+                    backgroundColor: [
+                        documentStyle.getPropertyValue('--red-500'),
+                        documentStyle.getPropertyValue('--green-500'),
+                        documentStyle.getPropertyValue('--yellow-500'),
+                        documentStyle.getPropertyValue('--bluegray-500'),
+                        documentStyle.getPropertyValue('--blue-500'),
+                        documentStyle.getPropertyValue('--orange-500'),
+                        documentStyle.getPropertyValue('--purple-500'),
+                    ],
+                    label: 'My dataset'
+                }
+            ],
+            labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue']
+        };
+        const options = {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                r: {
+                    grid: {
+                        color: surfaceBorder
+                    }
+                }
+            }
+        };
+
+        setChartData(data);
+        setChartOptions(options);
+    }, []);
+
+
+
+
+    return <section className='FNL_CONT'>
+        <header className="NAV_SUP">
+            <nav className="NAV_Home">
+                <Button className="btn_link_1" label='Inicio' severity='secondary' text/>
+            </nav>
+
+            <nav className="NAV_Link">
+                <ul className='LNK_List'>
+                    <li className="btn_link">
+                        <Button className="btn_link_2" label='Ver stands' severity='secondary' text/>
+                    </li>
+                    <li className="btn_link">
+                        <Button className="btn_link_2" label='Ver estadísticas' severity='secondary' text/>
+                    </li>
+                    <li className="btn_link">
+                        <Button className="btn_link_2" label='Configuraciones' severity='secondary' text/>
+                    </li>
+                </ul>
+            </nav>
+
+            <nav className="NAV_Icon">
+                <ul className="ICN_List">
+                    <li className="btn_link_3">
+                        <Button icon="pi pi-user" aria-label="User"/>
+                    </li>
+                    <li className="btn_link_3">
+                        <Button icon="pi pi-bell" severity="warning" aria-label="Notification"/>
+                    </li>
+                    <li className="btn_link_3">
+                        <Button icon="pi pi-times" severity="danger" aria-label="Cancel"/>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <Divider layout='horizontal'/>
+
+        <section className="SCT_CONT">
+            <section className="SCT_INFO">
+
+            </section>
+
+            <Divider layout='vertical'/>
+
+            <section className="SCT_GRAPH">
+                <div className="card">
+                    <Chart type="polarArea" data={chartData} options={chartOptions} id='graph_element'/>
+                </div>
+            </section>
+        </section>
+    </section>
 }
